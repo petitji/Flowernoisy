bc02a3338eab9f438646c439d6cc00ebae2e9a7e
Jihyeon <jhwithboa@gmail.com>
2019-03-23 18:39:25 +0100

    minus one completed

--- MinusOne.js
+++ MinusOne.js
@@ -33,6 +33,11 @@ function createMinusOneSet(){
     return new MinusOne(createCardSet(), createCardSet());
 }
 
+function createSameCardSet(){
+    var cardSet = createCardSet();
+    return new MinusOne(cardSet, cardSet);
+}
+
 function CheatCards(){
     var cardsArray = new Array();
     cardsArray.push(new MinusOne(new CardSet(Enum.MinusOneCard.Rock, Enum.MinusOneCard.Rock), new CardSet(Enum.MinusOneCard.Scissor, Enum.MinusOneCard.Scissor)));
@@ -42,7 +47,7 @@ function CheatCards(){
     return cardsArray[randomNum];
 }
 
-function LuckCards(){
+function LuckyUser1Cards(){
     var cardsArray = new Array();
     cardsArray.push(new MinusOne(createCardSet(), new CardSet(Enum.MinusOneCard.Scissor, Enum.MinusOneCard.Scissor)));
     cardsArray.push(new MinusOne(createCardSet(), new CardSet(Enum.MinusOneCard.Paper, Enum.MinusOneCard.Paper)));
@@ -51,47 +56,87 @@ function LuckCards(){
     return cardsArray[randomNum];
 }
 
-async function isUserCheated(userId){
-    var isCheated = await ServerAPI.getGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.Cheat);
-    console.log(isCheated);
-    return isCheated;
+function LuckyUser2Cards(){
+    var cardsArray = new Array();
+    cardsArray.push(new MinusOne(new CardSet(Enum.MinusOneCard.Scissor, Enum.MinusOneCard.Scissor), createCardSet()));
+    cardsArray.push(new MinusOne(new CardSet(Enum.MinusOneCard.Paper, Enum.MinusOneCard.Paper), createCardSet()));
+    cardsArray.push(new MinusOne(new CardSet(Enum.MinusOneCard.Rock, Enum.MinusOneCard.Rock), createCardSet()))
+    var randomNum = Utils.getRandomInt(0, 2);
+    return cardsArray[randomNum];
 }
 
-async function getCheatCard(gambleId, userId){ 
+async function checkIfCheated(userId, gambleId){
+    let isCheatSuccessful = await ServerAPI.getGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.Cheat);
+    return isCheatSuccessful == 1;
+}
+
+async function isCheatSuccessful(userId, gambleId){
     let serverUser1=  await ServerAPI.getUser(userId);
     var user =  await User.convertToUser(serverUser1);
     var userCheat = await user.calculateCheat();
 
     let luckNow = Utils.getRandomInt(1,100);
-    console.log(userId+ "의 현재운/속임수성공 확률 : " + luckNow  + "/" + userCheat);
+    console.log(userId + "의 현재운/속임수확률 : " + luckNow  + "/" + userCheat);
     
     if(luckNow < userCheat){
         console.log(userId + "은 속임수에 성공했다!");
-        
-        var cards = CheatCards();
         await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.Cheat, 1);
-        return cards;
+        return;
     }
     
     console.log(userId + "은 속임수에 실패했다.");
-    return createMinusOneSet();
+    await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.Cheat, 0);
+    return;
 }
 
-async function getCard(gambleId, userId){
-    let serverUser1=  await ServerAPI.getUser(userId);
-    var user =  await User.convertToUser(serverUser1);
-    var userLuck = await user.calculateLucky();
+async function getCards(user1Id, user2Id){
+    let serverUser1=  await ServerAPI.getUser(user1Id);
+    var user1 =  await User.convertToUser(serverUser1);
+    var user1Luck = await user1.calculateLucky();
 
-    let luckNow = Utils.getRandomInt(1,100);
-    console.log(userId+ "의 현재운/운좋을 확률 : " + luckNow  + "/" + userLuck);
+    let luckNow1 = Utils.getRandomInt(1,100);
+    console.log(user1Id+ "의 현재운/운좋을 확률 : " + luckNow1  + "/" + user1Luck);
+
+    let serverUser2 = await ServerAPI.getUser(user2Id);
+    var user2 =  await User.convertToUser(serverUser2);
+    var user2Luck = await user2.calculateLucky();
+
+    let luckNow2 = Utils.getRandomInt(1,100);
+    console.log(user2Id+ "의 현재운/운좋을 확률 : " + luckNow2  + "/" + user2Luck);
     
-    if(luckNow < userLuck){
-        console.log(userId + "은 운이 좋았다!");
-        return LuckCards();
+    //둘다 운이 좋을 경우
+    if(luckNow1 < user1Luck && luckNow2 < user2Luck){
+        console.log(user1Id + "은 운이 좋았다!");
+        console.log(user2Id + "은 운이 좋았다!");
+        return createSameCardSet();
+    //한명만 운이 좋을 경우
+    }else if(luckNow1 < user1Luck){
+        console.log(user1Id + "은 운이 좋았다!");
+        return LuckyUser1Cards();
+    }else if(luckNow2 < user2Luck){
+        console.log(user2Id + "은 운이 좋았다!");
+        return LuckyUser2Cards();
+    }else{
+        console.log(user1Id + "은 운이 좋지 않았다.");
+        console.log(user2Id + "은 운이 좋지 않았다.");
+        return createMinusOneSet();
     }
-    
-    console.log(userId + "은 운이 좋지 않았다.");
-    return createMinusOneSet();
+}
+
+async function getPastCard(gambleId, userId){
+    let cards = await ServerAPI.getGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.GivePlayerCard);
+    return Utils.toArray(cards);
+}
+
+function removeCard(pastCards, card){
+    var index = pastCards.findIndex(x => x.trim() == card);
+    pastCards.splice(index, 1);
+    return pastCards.toString();
+}
+
+async function getLeftCard(gambleId, userId){
+    let leftCard = await ServerAPI.getGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.SetPlayerCard);
+    return leftCard.trim();
 }
 
 module.exports.startMinusOne = async function (user1Id, user2Id, isVoteGamble){
@@ -99,34 +144,87 @@ module.exports.startMinusOne = async function (user1Id, user2Id, isVoteGamble){
 }
 
 module.exports.betChip = async function(gambleId, userId, chipCount){
-    await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.ESPActivity.BetChip, chipCount);
+    await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.BetChip, chipCount);
 }   
 
-module.exports.cheat = async function (gambleId, me, opponent){
-    if(isUserCheated(opponent)){
-        
-    }
+module.exports.cheat = async function(gambleId, userId){
     var statusCode = await ServerAPI.updateCheatCount(userId, gambleId);
     if(statusCode != 204){
+        console.log("속임수 횟수가 없다.");
         return;
     }
-
-    var card = await getCheatCard(gambleId, userId);
-    await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.GivePlayerCard, card);
-    return 
+    return await isCheatSuccessful(userId, gambleId);
 }
 
-module.exports.cards = async function(gambleId, userId){
-    return await getCard(gambleId, userId);
+module.exports.cards = async function(gambleId, user1, user2){
+    var isUser1Cheated  = await checkIfCheated(user1, gambleId);
+    var isUser2Cheated  = await checkIfCheated(user2, gambleId);
+
+    //둘 다 속임수를 쓴 경우
+    if(isUser1Cheated && isUser2Cheated){
+        var cards = createSameCardSet();
+        await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, user1, Enum.MinusOneActivity.GivePlayerCard, cards.user1.toString());
+        await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, user2, Enum.MinusOneActivity.GivePlayerCard, cards.user2.toString());
+        return cards;
+    }
+    //속임수가 있으면 속임수를 리턴
+    else if(isUser1Cheated){
+        var cards = CheatCards();
+        await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, user1, Enum.MinusOneActivity.GivePlayerCard, cards.user1.toString());
+        await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, user2, Enum.MinusOneActivity.GivePlayerCard, cards.user2.toString());
+        return cards;
+    }else if(isUser2Cheated){
+        var cards = CheatCards();
+        await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, user1, Enum.MinusOneActivity.GivePlayerCard, cards.user2.toString());
+        await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, user2, Enum.MinusOneActivity.GivePlayerCard, cards.user1.toString());
+        return cards;
+    }else{
+        var cards = await getCards(user1, user2);
+        await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, user1, Enum.MinusOneActivity.GivePlayerCard, cards.user1.toString());
+        await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, user2, Enum.MinusOneActivity.GivePlayerCard, cards.user2.toString());
+        return cards;
+    }
 }
 
-//속임수일 때 index 가 0이면 이기는 카드 , 1이 지는 카드
-module.exports.giveCard = async function(gambleId, cards, userId, index){
-    var cards = index > 0 ? cards.user2 : cards.user1;
-    await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.GivePlayerCard, JSON.stringify(cards));
-    return cards.toString();
+module.exports.submit = async function (gambleId, userId, card){
+    var pastCard = await getPastCard(gambleId, userId);
+    
+    if(!pastCard.find(element => element.trim() == card)){
+        console.log("현재 가지고 있는 카드와 맞지 않습니다.");
+        return false;
+    };
+
+    var leftCard = removeCard(pastCard, card);
+    await ServerAPI.createGambleLog(gambleId, Enum.GambleKind.MinusOne, userId, Enum.MinusOneActivity.SetPlayerCard, leftCard);
+    return leftCard;
+}
+
+module.exports.getWinner = async function (gambleId, user1, user2){
+    var user1Card = await getLeftCard(gambleId, user1);
+    var user2Card = await getLeftCard(gambleId, user2);
+
+    if(user1Card.win(user2Card)){
+        return user1;
+    }else if(user2Card.win(user1Card)){
+        return user2;
+    }else{
+        return 0;
+    }
 }
 
 module.exports.finishMinusOne = async function (gambleId, winnerId){
     await ServerAPI.updateGamble(gambleId, Enum.GambleKind.MinusOne, winnerId);
 }
+
+String.prototype.win = function win(user2Card) {
+    if((this == "보" && user2Card == "바위") || this == "가위" && user2Card == "보" || this == "바위" && user2Card == "가위"){
+        return true;
+    }
+    if((this == "보" && user2Card == "보") || this == "가위" && user2Card == "가위" || this == "바위" && user2Card == "바위"){
+        return 0;
+    }
+    if((this == "보" && user2Card == "가위") || this == "가위" && user2Card == "바위" || this == "바위" && user2Card == "보"){
+        return false;
+    }
+    return false;
+};
--- bot.js
+++ bot.js
@@ -3,8 +3,6 @@ var ChinchiroDice = require('./chinchiro.js');
 var ESP = require('./ESP.js');
 var MinusOne = require('./MinusOne.js');
 
-var ServerApi = require('./serverApi.js');
-
 //모든 boolean 값들은 1을 true로 합니다.
 //is 등으로 시작하는 변수들
 
@@ -41,21 +39,18 @@ async function chinchiroMain(){
     //await ESP.finishESP(25, user1Id);
 
     //var gamble = await MinusOne.startMinusOne(user1Id, user2Id, isVoteGamble); 
-    //var cheatCards = await MinusOne.cheat(23, user1Id, user2Id);
-    //var user1Card = await MinusOne.giveCard(19, cheatCards, user1Id, 0);
-    // var user2Card = await MinusOne.giveCard(19, cheatCards, user2Id, 1);
-    
-    //둘다 운이 좋았을 경우?
-    //  var cards = await MinusOne.card(19, user1Id);
-    //  console.log(cards);
-    //  var user1Card = await MinusOne.giveCard(cards, user1Id, 0);
-    //  var user2Card = await MinusOne.giveCard(cards, user2Id, 1);
-
-    // await MinusOne.submitUserCard(gambleId, user1Id, card);
-    // await MinusOne.submitUserCard(gambleId, user2Id, card);
-
-    // var winner = await ESP.getWinner(20, user1Id, user2Id);
-    // await ESP.finishESP(20, user1Id);
+    //await MinusOne.cheat(27, user1Id);
+    //await MinusOne.cheat(27, user2Id);
+    //await MinusOne.betChip(27, user1Id, 3);
+    //await MinusOne.betChip(27, user2Id, 5);
+    //var cards = await MinusOne.cards(27, user1Id, user2Id);
+    //console.log(cards.user1.toString())
+    //console.log(cards.user2.toString());
+    //await MinusOne.submit(27, user1Id, "보");
+    //await MinusOne.submit(27, user2Id, "보");
+    //var winner = await MinusOne.getWinner(27, user1Id, user2Id);
+    //if(winner ==0) 다시 진행
+    //await MinusOne.finishMinusOne(27, user1Id);
 
 }
 
